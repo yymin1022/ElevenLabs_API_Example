1d60850e5fcc091e6bb841ca12d5e6b03fb1c490
Useful <useful@vimosoft.com>
2026-04-09 11:00:15 +0900

    Update main : Added elevenlabs initialize logic

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import os
 
 from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
 
 # Initialize env variables from .env
 load_dotenv()
 ELEVEN_LABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY")
 
 
+def init_elevenlabs_instance():
+    """
+    Initialize ElevenLabs instance with API Key
+    """
+    return ElevenLabs(api_key=ELEVEN_LABS_API_KEY)
+
+
 def main():
     """
     Main function
     """
-    print("Hello, World!")
+    # Init elevenlabs instance
+    instance = init_elevenlabs_instance()
 
 
 if __name__ == "__main__":
