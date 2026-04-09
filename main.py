44760429a782778ef667bfa5ee3a765c65e17a77
Useful <useful@vimosoft.com>
2026-04-09 10:51:51 +0900

    Update main : Added script for env variables

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+# Initialize env variables from .env
+load_dotenv()
+ELEVEN_LABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY")
+
+
 def main():
     """
     Main function
